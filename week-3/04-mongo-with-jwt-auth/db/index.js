57916c5834d010e40a4b929d231ecfe29e163260
Jaideep1702 <jaideep.jambhale@gmail.com>
2024-05-01 02:44:24 +0530

    completed week 3 and 4

---
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://jaideepjambhale15856:1A3cXPOibhfTgLF7@cluster0.cke7tz9.mongodb.net/');
+
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
